6542546e5eb9f55ae6f7542cbbf63fb7a16542eb
Vu Hoang PHAN <vuhoangphan1307@gmail.com>
2022-06-17 18:21:45 +0200

    remove comment

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from cli import cli
-from isbnlib import meta
-# 978-91-27-16328-7
-# 978604-589243-5
 
 if __name__ == '__main__':
-    # print(meta("978604-589243-5", service="goob"))
     cli.cli()
